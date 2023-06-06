78dff4d4a9a1f2e1fc525e21dc1fa9cfa3d9ab0f
eva miliczka <eva.miliczka@gmail.com>
2023-06-06 16:46:20 +0200

    Refactoring finished

--- index.js
+++ index.js
@@ -1,31 +1,7 @@
-const mongoose = require('mongoose');
-const cors = require('cors');
-const express = require('express');
-const { PORT, MONGODB_URI } = require('./utils/config');
+const app = require('./app');
+const { PORT } = require('./utils/config');
+const { info } = require('./utils/logger');
 
-const app = express();
-const blogsRouter = require('./controllers/blogs');
-
-
-require ('dotenv').config();
-// console.log('Process env: ', process.env);
-
-const { info, error} = require('./utils/logger'); // error not in use
-
-mongoose.connect(MONGODB_URI)
-.then(() => {
-  // replaced result by ()
-  info('Connected to MongoDB');
-})
-.catch((err) => {
-  error('Error connecting to MongoDB: ', err.message);
-});;
-info('Connecting to ', MONGODB_URI);
-
-
-app.use(cors());
-app.use(express.json());
-app.use('/api/blogs', blogsRouter);
 
 
 app.listen(PORT, () => {
