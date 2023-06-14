b44d89831cf186e8e5e78b33ccf2047127d4cea0
eva miliczka <eva.miliczka@gmail.com>
2023-06-14 15:41:18 +0200

    ex 4.9 all blogs have ids

--- controllers/blogs.js
+++ controllers/blogs.js
@@ -1,28 +1,21 @@
+require('express-async-errors');
+
 const blogsRouter = require('express').Router();
 const Blog = require('../models/blog');
-const { info, error } = require('../utils/logger');
+const { info } = require('../utils/logger');
 
 info('This is blogs.js');
 
-blogsRouter.get('/', (request, response) => {
-  Blog
-    .find({})
-    .then(blogs => {
-      response.json(blogs)
-    })
-})
+blogsRouter.get('/', async (request, response) => {
+  const blogs = await Blog.find({});
+  response.json(blogs);
+});
 
-blogsRouter.post('/', (request, response) => {
+blogsRouter.post('/', async (request, response) => {
   const blog = new Blog(request.body);
 
-  blog
-    .save()
-    .then(result => {
-      response.status(201).json(result)
-    })
-    .catch(err => {
-      error(err.message);
-    })
+  const savedBlog = await blog.save();
+  response.status(201).json(savedBlog);
 });
 
 module.exports = blogsRouter;
--- tests/blog_api.test.js
+++ tests/blog_api.test.js
@@ -1,5 +1,8 @@
 const mongoose = require('mongoose');
 const supertest = require('supertest');
+
+mongoose.set("bufferTimeoutMS", 30000);
+
 const app = require('../app');
 const Blog = require('../models/blog');
 
@@ -32,19 +35,35 @@ beforeEach(async () => {
   const blogObjects = initialBlogs.map((blog) => new Blog(blog));
   const promiseArray = blogObjects.map((blog) => blog.save());
   await Promise.all(promiseArray);
-});
+}, 100000);
 
 test('blogs are returned as json ', async () => {
   await api
     .get('/api/blogs')
     .expect(200)
     .expect('Content-Type', /application\/json/);
-});
+}, 100000);
 
 test('the number of blogs in the DB is tha same as the number of initialBlogs', async () => {
   const response = await api.get('/api/blogs');
   expect(response.body).toHaveLength(initialBlogs.length);
-});
+}, 100000);
+
+test('all blogs have id', async () => {
+  const response = await api.get('/api/blogs');
+  // all elemnts have id
+  response.body.forEach(blog => expect(blog.id).toBeDefined());
+}, 100000);
+
+
+test('all ids are different', async () => {
+  const response = await api.get('/api/blogs');
+  const blogIds = response.body.map(blog => blog.id);
+  console.log(blogIds);
+  // set only contins unique values, so we can check if all ids are different
+  const setOfUniqueIds = new Set(blogIds);
+  expect(blogIds).toHaveLength(setOfUniqueIds.size);
+}, 100000);
 
 // app.js does not close the server and we do not need it now
 afterAll(async () => {
